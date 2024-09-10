8741300597e98c04f9343b25e6e62b95d9971076
Nevado <simon.tum@outlook.com>
2024-09-10 12:53:23 -0300

    class ended whoops

---
+++ lista2_ex5.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main() { 
+    char product[5][10] = {"car", "shampoo", "PC", "drugs", "plushie"};
+    float price[5] = {25000.99, 9.43, 3800, 1234.56, 2.50000};
+    return 0;
+}
